d787d48b44d4b8378369152b44d7753da8aa2928
Renilson Moreira <renilsonmoreira_@hotmail.com>
2022-09-07 17:51:50 -0300

    ajuste em callbacks

--- BluetoothApp/Views/ViewController.swift
+++ BluetoothApp/Views/ViewController.swift
@@ -8,66 +8,83 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController, CBCentralManagerDelegate {
+class ViewController: UIViewController {
 
     private var manager: CBCentralManager?
     private lazy var rootView = HomeView()
+    private var bluefruitPeripheral: CBPeripheral?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         manager = CBCentralManager(delegate: self, queue: nil)
         manager?.delegate = self
-        UIApplication.shared.isIdleTimerDisabled = true
     }
 
     override func loadView() {
         super.loadView()
         view = rootView
     }
-
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        
-        var consoleMsg = ""
-        
-        switch (central.state) {
-        case .poweredOff:
-            consoleMsg = "-> Bluetooth is powered off"
-        case .unknown:
-            consoleMsg = "-> Bluetooth is unknown"
-        case .resetting:
-            consoleMsg = "-> Bluetooth resetting"
-        case .unsupported:
-            consoleMsg = "-> Bluetooth unsupported"
-        case .unauthorized:
-            consoleMsg = "-> Bluetooth unauthorized"
-        case .poweredOn:
-            consoleMsg = "-> Bluetooth is powered on"
-        @unknown default:
-            break
-        }
-        
-        print(consoleMsg)
-        
-        print(central.scanForPeripherals(withServices: nil, options: nil))
-    }
-
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("Periférico: \(String(describing: peripheral.name))")
-        //rootView.items = peripheral
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        if peripheral.state == .disconnected {
-//            UIApplication.shared.tim
-        }
-        
-        
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+}
+
+extension ViewController: CBCentralManagerDelegate {
     
+    // MARK: - Verifica o estado do bluetooth
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        var consoleMsg = ""
+        
+        switch (central.state) {
+        case .poweredOff: consoleMsg = "-> Bluetooth is powered off"
+        case .unknown: consoleMsg = "-> Bluetooth is unknown"
+        case .resetting: consoleMsg = "-> Bluetooth resetting"
+        case .unsupported: consoleMsg = "-> Bluetooth unsupported"
+        case .unauthorized: consoleMsg = "-> Bluetooth unauthorized"
+        case .poweredOn: consoleMsg = "-> Bluetooth is powered on"
+        @unknown default:
+            break
+        }
+        print(consoleMsg)
+        
+        print(central.scanForPeripherals(withServices: nil, options: nil))
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name == "[TV] Samsung AU7700 50 TV"{
+            
+            ///interrompe o escaneamento
+            manager?.stopScan()
+            
+            /// Atribui ao o periferico selecionado
+            bluefruitPeripheral = peripheral
+            
+            ///Conecta com o disposivo
+            manager?.connect(bluefruitPeripheral ?? peripheral)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+       print("desconectado")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("conectado")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("conexao falhou")
+    }
+    
+}
+
+extension ViewController: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        print(service.peripheral?.name)
+    }
 }
 
